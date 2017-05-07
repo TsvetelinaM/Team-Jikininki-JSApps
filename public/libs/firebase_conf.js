f1bd11748571eec77207baa7a0c0dfafb49b9835
TsvetelinaM <tsvetka_ta@abv.bg>
2017-05-07 23:47:40 +0300

    UI updated

---
+++ public/libs/firebase_conf.js
@@ -0,0 +1,11 @@
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBelM-Jn_TCl27vG3Mi6hUg09i1YphWj38",
+    authDomain: "jikininki-b48ae.firebaseapp.com",
+    databaseURL: "https://jikininki-b48ae.firebaseio.com",
+    projectId: "jikininki-b48ae",
+    storageBucket: "jikininki-b48ae.appspot.com",
+    messagingSenderId: "244661341178"
+  };
+  firebase.initializeApp(config);
